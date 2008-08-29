cc439411ed3a1a84aa52d5a4a1458d73739587f1
Bryce Kerley <bryce@worldmedia.net>
2008-08-29 11:28:03 -0400

    fail a simple test

--- test/test_rules.rb
+++ test/test_rules.rb
@@ -15,5 +15,15 @@ class TestRules < Test::Unit::TestCase
     should 'turn into a rulelist' do
       assert_kind_of RuleList, RuleList.new(@filenode.selection_array)
     end
+    
+    context 'and the rulelist for that file' do
+      setup do
+        @rulelist = RuleList.new(@filenode.selection_array)
+      end
+      
+      should 'deny by default' do
+        assert_equal :deny, @rulelist.test('acl://nonesuch', 'somebody')
+      end
+    end
   end
 end
